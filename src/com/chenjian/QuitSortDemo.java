e2a66f3f2725f08074a9c32313d03d992dab0f98
ChenJian17 <31843897+ChenJian17@users.noreply.github.com>
2021-06-24 08:32:39 +0800

    update:剑指offer两题

---
+++ src/com/chenjian/QuitSortDemo.java
@@ -0,0 +1,14 @@
+package com.chenjian;
+
+/**
+ * Created by chenjian on 2021/3/13 19:08
+ */
+public class QuitSortDemo {
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{};
+
+
+    }
+}
--- src/com/chenjian/quitSort.java
+++
@@ -1,7 +0,0 @@
-package com.chenjian;
-
-/**
- * Created by chenjian on 2021/3/13 19:08
- */
-public class quitSort {
-}
