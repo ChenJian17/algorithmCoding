0a294e20512d9ab9bfb12244bcfd8df458bfc0e3
ChenJian17 <31843897+ChenJian17@users.noreply.github.com>
2021-06-25 19:11:13 +0800

    update:每日一题[6.25]

--- src/com/chenjian/QuitSortDemo.java
+++ src/com/chenjian/QuitSortDemo.java
@@ -5,6 +5,27 @@ package com.chenjian;
  */
 public class QuitSortDemo {
 
+    public void quickSort(int[] nums, int start, int end){
+        if (start >= end) return;
+        int left = start, right = end;
+        int pivot = nums[left];
+        while (left < right){
+            while (left < right && nums[right] >= pivot)
+                right --;
+            nums[left] = nums[right];
+            while (left < right && nums[left] <= pivot)
+                left ++;
+            nums[right] = nums[left];
+        }
+        nums[left] = pivot;
+        quickSort(nums, start, left-1);
+        quickSort(nums, left+1, end);
+    }
+
+
+
+
+
 
     public static void main(String[] args) {
         int[] nums = new int[]{};
