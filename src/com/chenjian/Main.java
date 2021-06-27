b824a7852c3f04a8685e8f847dd0bbc240d7a223
ChenJian17 <31843897+ChenJian17@users.noreply.github.com>
2021-06-27 15:07:56 +0800

    update:easy题...

--- src/com/chenjian/Main.java
+++ src/com/chenjian/Main.java
@@ -1,35 +1,96 @@
 package com.chenjian;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 //        final ThreadLocal<Main> holder = ThreadLocal.withInitial(Main::new);
-        new Main().largestOddNumber("239537672423884969653287101");
+        System.out.println(new Main().wonderfulSubstrings("aba"));
+
     }
 
-
-    public String largestOddNumber(String num) {
-        long sum = Long.valueOf(num);
-        if ((sum&1) == 1)
-            return num;
-        long res = 0;
-        for (int i = 0; i<num.length(); i++){
-            for (int j=i+1; j<num.length(); j++){
-                String str = num.substring(i,j);
-                long tmp = Long.valueOf(str);
-                if ((tmp&1) == 1){
-                    res = Math.max(res, tmp);
-                }
+    public boolean canBeIncreasing2(int[] nums) {
+        if (nums.length == 1)
+            return true;
+        if (nums.length == 2)
+            return true;
+        int flag = -1;
+        int temp = nums[0];
+        int others = Integer.MIN_VALUE;
+        for (int i=1; i<nums.length; i++){
+            if (nums[i] > temp){
+                temp = nums[i];
+            }else {
+                //大于等于 nums[i]<nums[i-1]
+//                temp = nums[i-1];
+//                temp = nums[i];
+                if(i>=2)
+                    others = nums[i-2];
+                flag = i;
+                break;
             }
         }
-        if (res == 0)
-            return "";
+        if (flag == -1)
+            return true;
         else
-            return String.valueOf(res);
-
-
+            return judge(nums, flag+1, nums[flag], others) || judge(nums, flag, nums[flag-1], others);
 
     }
+
+    public boolean judge(int[] nums, int k, int temp, int others){
+        if (nums[k] <= others)
+            return false;
+        for (int j = k; j<nums.length; j++){
+            if (nums[j] <= temp)
+                return false;
+            else
+                temp = nums[j];
+        }
+        return true;
+
+    }
+
+    long count = 0;
+
+    public long wonderfulSubstrings(String word) {
+        backtrace(word,"",0);
+        return count;
+
+    }
+
+    public void backtrace(String word, String str,int index){
+        if (!str.equals("")&& !isBeaut(str))
+            count++;
+        if (index == word.length())
+            return;
+
+        for (int i= index; i<word.length(); i++){
+            backtrace(word,str+word.charAt(i),i+1);
+        }
+
+    }
+
+    public boolean isBeaut(String str){
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i=0; i<str.length(); i++){
+            map.put(str.charAt(i),map.getOrDefault(str.charAt(i),0)+1);
+        }
+        int cnt = 0;
+        for (Map.Entry<Character,Integer> tmp: map.entrySet()){
+            if (tmp.getValue()%2 != 0)
+                cnt ++;
+        }
+        return cnt>1;
+
+    }
+
+
+
+
+
 
 }
