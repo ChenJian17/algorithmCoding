de288751280ba956e8d9fd38873eb46d2e0dbb79
ChenJian17 <31843897+ChenJian17@users.noreply.github.com>
2021-07-06 00:22:51 +0800

    update:反转链表的两种写法[头插法，尾插法]

---
+++ src/com/chenjian/cn/util/reverseList.java
@@ -0,0 +1,45 @@
+package com.chenjian.cn.util;
+
+import java.util.List;
+
+/**
+ * @BelongsProject: algorithmCoding
+ * @BelongsPackage: com.chenjian.cn.util
+ * @Author: Joker
+ * @CreateTime: 2021-07-05 23:47
+ * @Description: 反转链表
+ */
+public class reverseList {
+
+    //头插法，有点难理解
+    public ListNode reverse1(ListNode head){
+        ListNode headNode = new ListNode(-1);
+        headNode.next = null;
+        while (head != null){
+            ListNode tmp = head;
+            head = head.next;
+            tmp.next = headNode.next;
+            headNode.next = tmp;
+        }
+        return headNode.next;
+
+
+    }
+
+
+    //尾插法，也叫双指针迭代
+    public ListNode reverse2(ListNode head){
+        if (head == null) return null;
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+
+    //递归
+}
